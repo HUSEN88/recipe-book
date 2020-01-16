1db9e38b6cd08b7c7ea1ecb3b63b4066e9bc5ce9
husen88 <husains993@gmail.com>
2020-01-16 14:10:19 +0530

    chore:Firebas integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDo6HCiw6Ij5c7Gv9fzMbPboW7sr608910',
+    authDomain: 'recipe-book-6114d.firebaseapp.com',
+    databaseURL: 'https://recipe-book-6114d.firebaseio.com',
+    projectId: 'recipe-book-6114d',
+    storageBucket: 'recipe-book-6114d.appspot.com',
+    messagingSenderId: '865373118709',
+    appId: '1:865373118709:web:731e139e502a4f5d61e5bc'
+  }
 };
